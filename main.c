0ee3c54f8d480a6943651023e338e9389c7b3302
Sebastian Valentin Telescu <stelescu@c1r5s3.42madrid.com>
2026-06-08 17:55:20 +0200

    cleaning main

--- main.c
+++ main.c
@@ -1,28 +1,20 @@
 #include "push_swap.h"
 
-void	liberty(t_stack *a, t_stack *b, char *joined, char **splited)
+int	liberty(t_stack *a, t_stack *b, char *joined, char **splited)
 {
 	destroy_stack(a);
 	destroy_stack(b);
 	free(joined);
 	free_split(splited);
+	return (1);
 }
 
-static t_stack	*init_stack(t_option *op, char **av, int ac,
-					char ***splited, char **joined)
+static int	init_stack(char ***av, int ac, char **joined, t_option *op)
 {
-	int	num_count;
-
-	*joined = join_args(av + 1, ac - 1);
-	if (!*joined)
-		return (NULL);
-	num_count = ft_count_words(*joined, ' ');
-	*splited = ft_split(*joined, ' ');
-	if (!*splited || num_count < 2)
-		return (NULL);
-	if (ft_skip(op, *splited, num_count) == -1)
-		return (NULL);
-	return (ft_verfier(*splited, num_count, op));
+	*joined = join_args(*av + 1, ac - 1);
+	ac = ft_count_words(*joined, ' ');
+	*av = ft_split(*joined, ' ');
+	return (ft_skip(op, *av, ac));
 }
 
 int	main(int ac, char **av)
@@ -30,35 +22,27 @@ int	main(int ac, char **av)
 	t_stack		*a;
 	t_stack		*b;
 	t_option	op;
-	char		*joined;
-	char		**splited;
-	int			num_count;
 	int			skip;
+	char		*joined;
 
-	joined = join_args(av + 1, ac - 1);
-	num_count = ft_count_words(joined, ' ');
-	splited = ft_split(joined, ' ');
-	skip = ft_skip(&op, splited, num_count);
-	if (!splited || !joined || num_count < 2 || skip == -1)
-		return (ft_error(NULL, NULL));
-	a = ft_verfier(splited, num_count, skip);
-	if (!a)
+	skip = init_stack(&av, ac, &joined, &op);
+	a = ft_verfier(av, ac, skip);
+	if (!av || !joined || ac < 2 || !a || !skip)
 		return (ft_error(NULL, NULL));
 	if (a->top < 1 || ft_is_sorted(a))
 	{
 		destroy_stack(a);
 		return (0);
 	}
-	b = create_stack(num_count);
+	b = create_stack(ac);
 	if (!b)
 	{
 		free(joined);
-		free_split(splited);
+		free_split(av);
 		return (ft_error(a, NULL));
 	}
 	decide_algorithm(a, b, &op, ac -2);
-	liberty(a, b, joined, splited);
-	return (0);
+	return (liberty(a, b, joined, av));
 }
 
 
